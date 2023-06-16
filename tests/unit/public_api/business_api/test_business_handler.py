8de36b47c8cea8114782edd716f919529c651f90
theo <theo.frederichs@binary-butterfly.de>
2023-06-16 11:53:29 +0200

    Added a test for the handler for searching strings

--- tests/unit/public_api/business_api/test_business_handler.py
+++ tests/unit/public_api/business_api/test_business_handler.py
@@ -2,7 +2,7 @@ import pytest
 from unittest.mock import Mock
 
 import webapp.common.config
-from webapp.public_api.business_api.business_handler import BusinessHandler
+from webapp.public_api.business_api.business_handler import BusinessHandler, BusinessSearchQuery
 from webapp.repositories.business_repository import BusinessRepository
 
 
@@ -10,16 +10,37 @@ from webapp.repositories.business_repository import BusinessRepository
 def business_repository_mock():
     return Mock(BusinessRepository)
 
+
+@pytest.fixture
+def business_handler_mock():
+    return Mock(BusinessHandler)
+
+
 @pytest.fixture
 def config_helper_mock():
     return Mock(webapp.common.config.ConfigHelper)
+
 
 @pytest.fixture
 def logger_mock():
     return Mock(webapp.common.logger.Logger)
 
-def test_business_handler_get_business_by_id(business_repository_mock,logger_mock,config_helper_mock):
+
+@pytest.fixture
+def business_querry_mock():
+    return Mock(BusinessSearchQuery(name="EN"))
+
+
+def test_business_handler_get_business_by_id(business_repository_mock, logger_mock, config_helper_mock):
     business_repository_mock.fetch_by_id.return_value = "teststring"
-    handler = BusinessHandler(business_repository=business_repository_mock, logger=logger_mock, config_helper=config_helper_mock)
+    handler = BusinessHandler(business_repository=business_repository_mock, logger=logger_mock,
+                              config_helper=config_helper_mock)
     assert handler.get_business_by_id(1) is "teststring"
     business_repository_mock.fetch_by_id.assert_called_with(1)
+
+
+def test_business_handler_get_by_name(business_repository_mock, business_querry_mock, logger_mock, config_helper_mock):
+    business_repository_mock.fetch_business_by_name.return_value = "teststring"
+    handler = BusinessHandler(business_repository=business_repository_mock, logger=logger_mock,
+                              config_helper=config_helper_mock)
+    assert handler.get_business_by_name(BusinessSearchQuery(name="En")) is "teststring"
