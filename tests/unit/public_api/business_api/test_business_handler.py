7ac1338efec58f3656d2f4fc0191990aa262a646
theo <theo.frederichs@binary-butterfly.de>
2023-09-07 12:44:22 +0200

    removed test for previous removed method

--- tests/unit/public_api/business_api/test_business_handler.py
+++ tests/unit/public_api/business_api/test_business_handler.py
@@ -38,9 +38,3 @@ def test_business_handler_get_business_by_id(business_repository_mock, logger_mo
     assert handler.get_business_by_id(1) is "teststring"
     business_repository_mock.fetch_by_id.assert_called_with(1)
 
-
-def test_business_handler_get_by_name(business_repository_mock, business_query_mock, logger_mock, config_helper_mock):
-    business_repository_mock.fetch_business_by_name.return_value = "teststring"
-    handler = BusinessHandler(business_repository=business_repository_mock, logger=logger_mock,
-                              config_helper=config_helper_mock)
-    assert handler.get_business_by_name(BusinessSearchQuery(name="En")) is "teststring"
