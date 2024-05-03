13af5badb47df322ee6f46b24fffe570944bf723
theo <theo.frederichs@binary-butterfly.de>
2024-05-03 11:37:32 +0200

    Searching evse only by uid now

--- webapp/services/pub_sub_services/subscribe_handler/pubsub_evse_handler.py
+++ webapp/services/pub_sub_services/subscribe_handler/pubsub_evse_handler.py
@@ -3,7 +3,7 @@ Giro-e OCPI
 Copyright (c) 2024, binary butterfly GmbH
 All rights reserved.
 """
-
+from webapp.common.events import EventHelper
 from webapp.models.evse import EvseStatus
 from webapp.repositories import ObjectNotFoundException, EvseRepository
 from webapp.services.pub_sub_services.subscribe_handler.pubsub_base_handler import PubSubBaseHandler
@@ -12,22 +12,16 @@ from webapp.services.pub_sub_services.subscribe_handler.pubsub_base_handler impo
 class PubSubEvseHandler(PubSubBaseHandler):
     evse_repository: EvseRepository
 
-    def __init__(self, *, evse_repository: EvseRepository, **kwargs):
+    def __init__(self, *, evse_repository: EvseRepository, event_helper: EventHelper, **kwargs):
         super().__init__(**kwargs)
         self.evse_repository = evse_repository
 
     def handle(self, object_uid: str, value: str):
         try:
-            evse = self.evse_repository.fetch_evse_by_evse_id(object_uid)
+            evse = self.evse_repository.fetch_by_id(object_uid)
         except ObjectNotFoundException:
             # If connector is not known to our system just do nothing
             self.logger.info('redis-pub-sub', f'unknown evse {object_uid} got status update to {value}')
-            return
-
-        # Just accept EVSE updates where we are the CPO
-        own_cpo_role = self.config_helper.get('OWN_CPO_CREDENTIALS_ROLE')
-        if not (evse.location.country_code == own_cpo_role['country_code'] and evse.location.party_id == own_cpo_role['party_id']):
-            self.logger.info('redis-pub-sub', f'ignoring evse {object_uid} as it\'s a foreign one')
             return
 
         try:
