53615666c318937772ca6c05b85ce1d1e3f9cbad
theo <theo.frederichs@binary-butterfly.de>
2024-05-03 11:36:58 +0200

    Searching evse by id now

--- webapp/services/pub_sub_services/subscrption_service.py
+++ webapp/services/pub_sub_services/subscrption_service.py
@@ -9,30 +9,28 @@ from butterfly_pubsub.asyncio import PubSubClient
 from webapp.repositories import EvseRepository
 from webapp.services.base_service import BaseService
 from .subscriber import PubSubSubscriber
+from webapp.dependencies import dependencies
 from ...common.config import ConfigHelper
-from ...common.events import EventHelper
+from ...common.events import EventHelper, event_helper
+from ...common.logger import Logger
 
 
 class PubSubService(BaseService):
-    subscriber: PubSubSubscriber
-    pubsub_client: PubSubClient
-    evse_repository: EvseRepository
 
     def __init__(
         self,
         evse_repository: EvseRepository,
         pubsub_client: PubSubClient,
-        event_helper: EventHelper,
-        config_helper: ConfigHelper,
         **kwargs,
     ):
         super().__init__(**kwargs)
 
+        self.event_helper = dependencies.get_event_helper()
         self.pubsub_client = pubsub_client
         self.subscriber = PubSubSubscriber(
             logger=self.logger,
-            event_helper=event_helper,
-            config_helper=config_helper,
+            config_helper=self.config_helper,
+            event_helper=self.event_helper,
             evse_repository=evse_repository,
         )
 
