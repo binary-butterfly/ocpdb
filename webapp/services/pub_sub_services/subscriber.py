c6d9ae17360acade3b28731f29461453060d19b8
theo <theo.frederichs@binary-butterfly.de>
2024-05-08 10:30:36 +0200

    Added eader

--- webapp/services/pub_sub_services/subscriber.py
+++ webapp/services/pub_sub_services/subscriber.py
@@ -1,7 +1,19 @@
 """
-Giro-e OCPI
-Copyright (c) 2024, binary butterfly GmbH
-All rights reserved.
+Open ChargePoint DataBase OCPDB
+Copyright (C) 2024 binary butterfly GmbH
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from typing import Dict
@@ -17,6 +29,7 @@ from webapp.repositories import EvseRepository, ObjectNotFoundException
 from webapp.dependencies import dependencies
 from webapp.services.pub_sub_services.subscribe_handler.pubsub_base_handler import PubSubBaseHandler
 from webapp.services.pub_sub_services.subscribe_handler.pubsub_evse_handler import PubSubEvseHandler
+from webapp.shared.evse.evse_mapper import EvseMapper
 
 
 class PubSubSubscriber(PubSubSubscriberParent):
@@ -24,7 +37,7 @@ class PubSubSubscriber(PubSubSubscriberParent):
     event_helper: EventHelper
     logger: Logger
     handler: Dict[str, PubSubBaseHandler]
-
+    evse_mapper: EvseMapper = EvseMapper()
     pattern = '*.*.STATUS'
 
     def __init__(
@@ -56,6 +69,7 @@ class PubSubSubscriber(PubSubSubscriberParent):
         message_type = channel_fragments[0]
         object_uid = channel_fragments[1]
         value = message['data'].decode().upper()
+        status = str(self.evse_mapper.map_charge_connector_status_to_evse_status(value))
         try:
 
             evse = self.evse_repository.fetch_only_by_uid(uid=object_uid)
@@ -64,20 +78,23 @@ class PubSubSubscriber(PubSubSubscriberParent):
             return
 
         try:
-            evse_status: EvseStatus = EvseStatus[value]
+            print(status)
+            evse_status: EvseStatus = EvseStatus[status]
+            print(evse_status)
         except KeyError:
             self.logger.info('redis-pub-sub', f' evse {object_uid} unknown status {value}')
             return
         self.logger.info(
             'redis-pub-sub',
             f'evse {object_uid} got status update from {object_uid} to {value}', )
-        if value == evse.status:
+        if status == evse.status:
             return
         evse.status = evse_status
+
         self.evse_repository.save_evse(evse)
 
         # Discard unsupported message types
         if message_type not in self.handler:
             return
 
-        self.handler[message_type].handle(evse.id, value)
+        self.handler[message_type].handle(evse.id, status)
