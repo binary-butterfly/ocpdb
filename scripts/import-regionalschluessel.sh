8278798a0a647770186be35ceab3e364191330d0
Ernesto Ruge <ernesto.ruge@binary-butterfly.de>
2026-03-10 17:57:07 +0100

    Smaller improvements
    Co-authored-by: Jannis <mail@jannisr.de>

--- scripts/import-regionalschluessel.sh
+++ scripts/import-regionalschluessel.sh
@@ -7,9 +7,9 @@ if [ -f /data/.vg25-imported ]; then
 fi
 
 if [ ! -f /data/vg25.gpkg ]; then
-    wget -q $VG22_GEOPACKAGE_URL -O /data/vg25.gpkg -U ${$VG25_GEOPACKAGE_WGET_USER_AGENT:-OCPDB}
+    wget -q "$VG22_GEOPACKAGE_URL" -O /data/vg25.gpkg -U "${VG25_GEOPACKAGE_WGET_USER_AGENT:-OCPDB}"
 fi
 
-ogr2ogr -f PostgreSQL "PG:host=$POSTGRES_HOST port=5432 user='$POSTGRES_USER' password='$POSTGRES_PASSWORD' dbname='ocpdb'" -nln regionalschluessel --config OGR_TRUNCATE YES -s_srs EPSG:25832 -t_srs EPSG:4326 /data/vg25.gpkg v_vg25_gem
+ogr2ogr -f PostgreSQL "PG:host='${PGHOST:?missing PGHOST variable}' port=${PGPORT:-5432} user='${PGUSER:?missing PGUSER variable}' password='${PGPASSWORD:?missing PGPASSWORD variable}' dbname='${PGDATABASE:-ocpdb}'" -nln regionalschluessel --config OGR_TRUNCATE YES -s_srs EPSG:25832 -t_srs EPSG:4326 /data/vg25.gpkg v_vg25_gem
 
 touch /data/.vg25-imported
